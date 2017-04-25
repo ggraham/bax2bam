ef6d67839d68c4e3d9fdcfffcc453468eb5e6676
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2017-04-25 13:24:27 -0700

    Making dependency visible on the plan

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -1,36 +1,19 @@
 #!/bin/bash -e
 rm -rf test-reports
-PBBAM=`/bin/ls -t tarballs/pbbam*tgz|head -1`
-curl -s -L http://nexus/repository/maven-snapshots/pacbio/sat/htslib/htslib-1.1-SNAPSHOT.tgz -o tarballs/htslib-1.1-SNAPSHOT.tgz
-HTSLIB=`/bin/ls -t tarballs/htslib-*.tgz|head -1`
-BLASR_LIBCPP=`/bin/ls -t tarballs/blasr_libcpp*tgz|head -1`
-HAVE_HTSLIB=$PWD/prebuilts/`basename $HTSLIB .tgz`
-HAVE_BLASR_LIBCPP=$PWD/prebuilts/`basename $BLASR_LIBCPP .tgz`
-HAVE_PBBAM=$PWD/prebuilts/`basename $PBBAM .tgz`
-mkdir -p \
-         $HAVE_HTSLIB \
-         $HAVE_BLASR_LIBCPP \
-         $HAVE_BLASR \
-         $HAVE_PBBAM
-tar zxf $HTSLIB -C $HAVE_HTSLIB
-tar zxf $PBBAM -C $HAVE_PBBAM
-tar zxf $BLASR_LIBCPP -C $HAVE_BLASR_LIBCPP
+
+mkdir -p prebuilts/libbzip2-1.0.6
+curl -sL http://ossnexus/repository/unsupported/pitchfork/gcc-4.9.2/libbzip2-1.0.6.tgz \
+| tar zxf - -C prebuilts/libbzip2-1.0.6
+if [ ! -e .distfiles/gtest/release-1.7.0.tar.gz ]; then
+  mkdir -p .distfiles/gtest
+  curl -sL http://ossnexus/repository/unsupported/distfiles/googletest/release-1.7.0.tar.gz \
+    -o .distfiles/gtest/release-1.7.0.tar.gz
+fi
 
 type module >& /dev/null || . /mnt/software/Modules/current/init/bash
 module load git/2.8.3
 module load gcc/4.9.2
 module load ccache/3.2.3
-if [ -d pitchfork ]; then
-    echo y | make -C pitchfork _startover
-    git -C pitchfork clean -xdf
-    git -C pitchfork checkout .
-    git -C pitchfork checkout master
-    git -C pitchfork pull --rebase
-    #git -C pitchfork checkout bax2bam_bam2bax
-else
-    git clone git@github.com:PacificBiosciences/pitchfork.git
-    #git -C pitchfork checkout bax2bam_bam2bax
-fi
 cat > pitchfork/settings.mk << EOF
 PREFIX            = $PWD/deployment
 DISTFILES         = $PWD/.distfiles
@@ -45,15 +28,13 @@ HAVE_NCURSES      = /mnt/software/n/ncurses/5.9
 HAVE_HDF5         = /mnt/software/a/anaconda2/4.2.0
 HAVE_OPENBLAS     = /mnt/software/o/openblas/0.2.14
 HAVE_CMAKE        = /mnt/software/c/cmake/3.2.2/bin/cmake
-#
-HAVE_HTSLIB       = $HAVE_HTSLIB
-HAVE_PBBAM        = $HAVE_PBBAM
-HAVE_BLASR_LIBCPP = $HAVE_BLASR_LIBCPP
+HAVE_LIBBZIP2     = $PWD/prebuilts/libbzip2-1.0.6
 #
 bax2bam_REPO      = $PWD/bax2bam
+htslib_REPO       = $PWD/htslib
+pbbam_REPO        = $PWD/pbbam
+blasr_libcpp_REPO = $PWD/blasr_libcpp
 EOF
-ls -l tarballs prebuilts
+echo y | make -C pitchfork _startover
 cd pitchfork
-cat settings.mk
-make bax2bam
-rm -rf tarballs prebuilts
+VERBOSE=1 make bax2bam -j8
